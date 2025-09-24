877f36c99822645cd91139fafbb027c23a4cb9b6
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 12:37:46 -0600

    Add RefreshTokenConverter for Firestore data conversion

---
+++ src/infrastructure/data/converters/refresh-token-converter.ts
@@ -0,0 +1,34 @@
+import RefreshToken from "@Domain/entities/refresh-token.entity";
+import { FirestoreDataConverter } from "firebase-admin/firestore";
+import FirestoreUtils from "../firestore.utils";
+
+/* eslint-disable require-jsdoc */
+export default class RefreshTokenConverter implements FirestoreDataConverter<RefreshToken> {
+  toFirestore(entity: RefreshToken): FirebaseFirestore.DocumentData {
+    return {
+      userId: entity.userId,
+      tokenHash: entity.tokenHash,
+      createdByIp: entity.createdByIp,
+      userAgent: entity.userAgent,
+      deviceName: entity.deviceName,
+      revokedAt: entity.revokedAt ? FirebaseFirestore.FieldValue.serverTimestamp() : null,
+      expiresAt: entity.expiresAt,
+      createdAt: FirebaseFirestore.FieldValue.serverTimestamp(),
+    };
+  }
+
+  fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot) {
+    const data = snapshot.data();
+    return new RefreshToken({
+      id: snapshot.id,
+      userId: data.userId,
+      tokenHash: data.tokenHash,
+      createdByIp: data.createdByIp,
+      userAgent: data.userAgent,
+      deviceName: data.deviceName,
+      revokedAt: FirestoreUtils.getDateFrom(data.revokedAt),
+      expiresAt: FirestoreUtils.getDateFrom(data.expiresAt) || new Date(0),
+      createdAt: FirestoreUtils.getDateFrom(data.createdAt) || new Date(0),
+    });
+  }
+}
