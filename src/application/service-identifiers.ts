b93517d835836532b311219e9d48657ddec7c8bd
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 14:38:21 -0600

    Add application service identifiers

---
+++ src/application/service-identifiers.ts
@@ -0,0 +1,5 @@
+export const SERVICE_IDENTIFIERS = {
+  IAuthService: Symbol.for("IAuthenticationService"),
+  ITokenService: Symbol.for("ITokenService"),
+  IPasswordHasher: Symbol.for("IPasswordHasherService"),
+};
