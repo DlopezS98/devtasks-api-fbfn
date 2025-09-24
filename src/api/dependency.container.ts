9d16441ae0d7e5b12848851fa0d6fa3c03567460
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 14:39:17 -0600

    load application services into the dependency container

--- src/api/dependency.container.ts
+++ src/api/dependency.container.ts
@@ -1,15 +1,18 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import InfrastructureContainerSetup from "@Infrastructure/infrastructure-container.setup";
+import ApplicationContainer from "@Application/application-container.setup";
 
 export default class DependencyContainer {
   private static instance: DependencyContainer;
   private readonly infrastructureServices: InfrastructureContainerSetup;
+  private readonly applicationServices: ApplicationContainer;
   public readonly container: Container;
 
   private constructor() {
     this.container = new Container({ defaultScope: "Request" });
     this.infrastructureServices = InfrastructureContainerSetup.getInstance(this.container);
+    this.applicationServices = ApplicationContainer.getInstance(this.container);
   }
 
   public static getInstance(): DependencyContainer {
@@ -21,5 +24,6 @@ export default class DependencyContainer {
 
   public initialize(): void {
     this.infrastructureServices.load();
+    this.applicationServices.load();
   }
 }
