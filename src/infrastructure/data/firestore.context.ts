c036157849fb4b5f4b5816c365cac7753f8f0877
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 11:59:57 -0600

    Refactor Firebase configuration: replace FirebaseConfig class with FirestoreContext for improved management of Firestore instances and collections

---
+++ src/infrastructure/data/firestore.context.ts
@@ -0,0 +1,57 @@
+import BaseEntity from "@Domain/entities/base-entity";
+import * as admin from "firebase-admin";
+import FactoryConverter from "./converters/factory-converter";
+// import serviceAccount from "./devtasks-serviceaccount.json";
+
+/**
+ * Firebase configuration class.
+ * It prevents the need for multiple imports of Firebase configuration throughout the codebase.
+ * @class FirebaseConfig
+ */
+export default class FirestoreContext {
+  private static instance: FirestoreContext;
+  private readonly fbApp: admin.app.App;
+
+  // eslint-disable-next-line require-jsdoc
+  private constructor() {
+    this.fbApp = admin.initializeApp({ credential: admin.credential.applicationDefault() }, "DevTasksApi");
+    // this.fbApp = admin.initializeApp(
+    //   { credential: admin.credential.cert(serviceAccount as admin.ServiceAccount) },
+    //   "DevTasksApi",
+    // );
+  }
+
+  /**
+   * Get the Firestore database instance.
+   * @return {FirebaseFirestore.Firestore} The Firestore database instance.
+   */
+  public firestore(): FirebaseFirestore.Firestore {
+    return this.fbApp.firestore();
+  }
+
+  /**
+   * Get the Firebase app instance.
+   * @return {admin.app.App} The Firebase app instance.
+   */
+  public static getInstance(): FirestoreContext {
+    if (!FirestoreContext.instance) {
+      FirestoreContext.instance = new FirestoreContext();
+    }
+    return FirestoreContext.instance;
+  }
+
+  /**
+   * Initialize and return the Firebase app instance.
+   * @return {admin.app.App} The initialized Firebase app instance.
+   */
+  public static initializeApp(): admin.app.App {
+    return FirestoreContext.getInstance().fbApp;
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  public getCollection<TEntity extends BaseEntity>(entity: TEntity): admin.firestore.CollectionReference<TEntity> {
+    return this.fbApp.firestore()
+      .collection(entity.namespace)
+      .withConverter(FactoryConverter.createConverter<TEntity>(entity));
+  }
+}
