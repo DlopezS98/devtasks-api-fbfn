e29f9ba5980f3f4d27f6068212889f408b9a445f
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 13:23:28 -0600

    Implement UserConverter in FactoryConverter for Firestore data conversion

--- src/infrastructure/data/converters/factory-converter.ts
+++ src/infrastructure/data/converters/factory-converter.ts
@@ -2,13 +2,16 @@
 import BaseEntity from "@Domain/entities/base-entity";
 import TaskConverter from "./task-converter";
 import { FirestoreDataConverter } from "firebase-admin/firestore";
+import UserConverter from "./user-converter";
+import RefreshTokenConverter from "./refresh-token-converter";
 
 /* eslint-disable require-jsdoc */
 export default class FactoryConverter {
   public static createConverter<TEntity extends BaseEntity>(entity: TEntity): FirestoreDataConverter<TEntity> {
     switch (entity.namespace) {
       case "Tasks": return new TaskConverter() as unknown as FirestoreDataConverter<TEntity>;
-      case "Users": throw new Error("UserConverter not implemented yet");
+      case "Users": return new UserConverter() as unknown as FirestoreDataConverter<TEntity>;
+      case "RefreshTokens": return new RefreshTokenConverter() as unknown as FirestoreDataConverter<TEntity>;
       default: throw new Error(`No converter found for entity namespace: ${entity.namespace}`);
     }
   }
