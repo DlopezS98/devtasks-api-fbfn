ce90da7a558f4d1a99066e72450cfd901e993941
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 12:37:40 -0600

    Add UserConverter for Firestore data conversion of User entities

---
+++ src/infrastructure/data/converters/user-converter.ts
@@ -0,0 +1,32 @@
+/* eslint-disable require-jsdoc */
+import User from "@Domain/entities/user";
+import { FirestoreDataConverter } from "firebase-admin/firestore";
+import FirestoreUtils from "../firestore.utils";
+
+export default class UserConverter implements FirestoreDataConverter<User> {
+  toFirestore(entity: User): FirebaseFirestore.DocumentData {
+    return {
+      email: entity.email,
+      displayName: entity.displayName,
+      passwordHash: entity.passwordHash,
+      passwordSalt: entity.passwordSalt,
+      isActive: entity.isActive,
+      createdAt: FirebaseFirestore.FieldValue.serverTimestamp(),
+      updatedAt: entity.updatedAt ? FirebaseFirestore.FieldValue.serverTimestamp() : null,
+    };
+  }
+
+  fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot) {
+    const data = snapshot.data();
+    return new User({
+      id: snapshot.id,
+      email: data.email,
+      displayName: data.displayName,
+      passwordHash: data.passwordHash,
+      passwordSalt: data.passwordSalt,
+      isActive: data.isActive,
+      createdAt: FirestoreUtils.getDateFrom(data.createdAt) || new Date(0),
+      updatedAt: FirestoreUtils.getDateFrom(data.updatedAt),
+    });
+  }
+}
