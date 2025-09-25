d6b2a3e7e33b4a8858b9c059ef352ccf93a646d1
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 22:47:16 -0600

    Refactor JwtOptions to a class and implement fromEnvironment method for better structure and environment integration

--- src/infrastructure/models/jwt-options.ts
+++ src/infrastructure/models/jwt-options.ts
@@ -1,8 +1,23 @@
-export interface JwtOptions {
-  issuer: string;
-  audience: string;
-  signingKey: string;
-  accessTokenMinutes: number;
-  refreshTokenDays: number;
-  serviceAccountEmail: string;
+import Environment from "src/environment";
+
+export class JwtOptions {
+  private constructor(
+    public readonly issuer: string,
+    public readonly audience: string,
+    public readonly signingKey: string,
+    public readonly accessTokenMinutes: number,
+    public readonly refreshTokenDays: number,
+    public readonly serviceAccountEmail: string
+  ) {}
+
+  static fromEnvironment(environment: Environment): JwtOptions {
+    return new JwtOptions(
+      environment.JWT_ISSUER,
+      environment.JWT_AUDIENCE,
+      environment.JWT_SIGNING_KEY,
+      environment.JWT_ACCESS_TOKEN_MINUTES,
+      environment.JWT_REFRESH_TOKEN_DAYS,
+      environment.SERVICE_ACCOUNT_EMAIL
+    );
+  }
 }
