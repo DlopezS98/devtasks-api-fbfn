13eb0324a04ee3f8cf956f439d9a291a73e6e061
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 23:26:52 -0600

    Initialize Express app and set up authentication routes with Dependency Injection

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,10 @@
 import express, { Express } from "express";
 import cors from "cors";
 
+import authRouterBuilder from "./routes/authentication.routes";
+import DependencyContainer from "./dependency.container";
+import AuthenticationController from "./controllers/authentication.controller";
+
 const app: Express = express();
 
 // Middlewares...
@@ -16,5 +20,8 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.send("Server is healthy...!");
 });
+
+const container = DependencyContainer.getInstance().initialize().getContainer();
+app.use(authRouterBuilder(container.get(AuthenticationController)));
 
 export default app;
