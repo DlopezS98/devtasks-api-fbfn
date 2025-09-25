920f05ea8ff9801f9c3de3ce1f079d075aab6a69
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 23:26:42 -0600

    Update authentication routes to include full path for login and register endpoints

--- src/api/routes/authentication.routes.ts
+++ src/api/routes/authentication.routes.ts
@@ -4,8 +4,8 @@ import AuthenticationController from "@Api/controllers/authentication.controller
 const authRouterBuilder = (controller: AuthenticationController) => {
   // eslint-disable-next-line new-cap
   const router = Router();
-  router.post("/login", controller.loginAsync.bind(controller));
-  router.post("/register", controller.registerAsync.bind(controller));
+  router.post("/authentication/login", controller.loginAsync.bind(controller));
+  router.post("/authentication/register", controller.registerAsync.bind(controller));
   return router;
 };
 
