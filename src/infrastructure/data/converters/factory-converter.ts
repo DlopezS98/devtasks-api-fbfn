bab617492173e65b4a02d3931400c5fdb5f02035
DlopezS98 <01dlopezs98@gmail.com>
2025-09-25 00:03:29 -0600

    Add LabelConverter case to FactoryConverter for Firestore data conversion

--- src/infrastructure/data/converters/factory-converter.ts
+++ src/infrastructure/data/converters/factory-converter.ts
@@ -5,14 +5,15 @@ import { FirestoreDataConverter } from "firebase-admin/firestore";
 import TaskConverter from "./task-converter";
 import UserConverter from "./user-converter";
 import RefreshTokenConverter from "./refresh-token-converter";
+import LabelConverter from "./label-converter";
 
-/* eslint-disable require-jsdoc */
 export default class FactoryConverter {
   public static createConverter<TEntity extends BaseEntity>(entity: TEntity): FirestoreDataConverter<TEntity> {
     switch (entity.namespace) {
       case "Tasks": return new TaskConverter() as unknown as FirestoreDataConverter<TEntity>;
       case "Users": return new UserConverter() as unknown as FirestoreDataConverter<TEntity>;
       case "RefreshTokens": return new RefreshTokenConverter() as unknown as FirestoreDataConverter<TEntity>;
+      case "Labels": return new LabelConverter() as unknown as FirestoreDataConverter<TEntity>;
       default: throw new Error(`No converter found for entity namespace: ${entity.namespace}`);
     }
   }
