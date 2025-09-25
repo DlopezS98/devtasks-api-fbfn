39bcedb43360af0568d4d2cdb80b65ad8a6e49ce
DlopezS98 <01dlopezs98@gmail.com>
2025-09-25 00:54:06 -0600

    Add labels routes to the application for label management

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import authRouterBuilder from "./routes/authentication.routes";
 import DependencyContainer from "./dependency.container";
 import AuthenticationController from "./controllers/authentication.controller";
+import labelsRouterBuilder from "./routes/labels.routes";
+import LabelsController from "./controllers/labels.controller";
 
 const app: Express = express();
 
@@ -23,5 +25,6 @@ app.get("/health", (req, res) => {
 
 const container = DependencyContainer.getInstance().initialize().getContainer();
 app.use("/api", authRouterBuilder(container.get(AuthenticationController)));
+app.use("/api", labelsRouterBuilder(container.get(LabelsController)));
 
 export default app;
