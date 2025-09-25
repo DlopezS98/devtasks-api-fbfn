0c069f9746662129cc9320f0f029c58f99e0a567
DlopezS98 <01dlopezs98@gmail.com>
2025-09-24 23:57:27 -0600

    Add Label entity with ILabelProps interface and constructor

---
+++ src/domain/entities/labels.entity.ts
@@ -0,0 +1,46 @@
+import BaseEntity from "./base-entity";
+
+export interface ILabelProps {
+  id: string;
+  name: string;
+  color: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  createdBy: string;
+  isActive: boolean;
+}
+
+export default class Label extends BaseEntity implements ILabelProps {
+  public get namespace(): string {
+    return "Labels";
+  }
+
+  name = "";
+  color = "";
+  createdAt!: Date;
+  updatedAt: Date | null = null;
+  createdBy = "";
+  isActive = true;
+
+  constructor(props: ILabelProps) {
+    super(props.id);
+    this.name = props.name;
+    this.color = props.color;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+    this.createdBy = props.createdBy;
+    this.isActive = props.isActive;
+  }
+
+  static empty(): Label {
+    return new Label({
+      id: "",
+      name: "",
+      color: "",
+      createdAt: new Date(),
+      updatedAt: null,
+      createdBy: "",
+      isActive: true,
+    });
+  }
+}
