f02dc3d0759ff70fc6b9a9abd2b4007aa1561ac8
DlopezS98 <01dlopezs98@gmail.com>
2025-09-25 00:10:24 -0600

    Add ILabelsService interface for label management operations

---
+++ src/application/abstractions/ilabels.service.ts
@@ -0,0 +1,8 @@
+import { BaseRequestDto } from "@Application/dtos/request/base-request.dto";
+import { LabelRequestDto } from "@Application/dtos/request/label.dto";
+import { LabelResponseDto } from "@Application/dtos/response/label.dto";
+
+export interface ILabelsService {
+  AddAsync(request: BaseRequestDto<LabelRequestDto>): Promise<LabelResponseDto>;
+  DeleteAsync(id: string): Promise<void>;
+}
