03be222b25ebdde1410ccde36826986c60318784
DlopezS98 <01dlopezs98@gmail.com>
2025-09-25 00:36:15 -0600

    Add ILabelsRepository interface and update LabelsRepository to use NormalizedName for query

---
+++ src/domain/abstractions/repositories/ilabels-repository.ts
@@ -0,0 +1,7 @@
+import Label from "@Domain/entities/labels.entity";
+
+import { IAsyncRepository } from "./iasync-repository";
+
+export interface ILabelsRepository extends IAsyncRepository<Label> {
+  getByNameAsync(name: string): Promise<Label | null>;
+}
--- src/infrastructure/data/repositories/labels.repository.ts
+++ src/infrastructure/data/repositories/labels.repository.ts
@@ -1,5 +1,6 @@
 import Label from "@Domain/entities/labels.entity";
 import { ILabelsRepository } from "@Domain/abstractions/repositories/ilabels-repository";
+import NormalizedName from "@Domain/value-objects/normalized-name";
 
 import FirestoreRepository from "./firestore.repository";
 import UnitOfWork from "./unit-of-work";
@@ -10,7 +11,7 @@ export default class LabelsRepository extends FirestoreRepository<Label> impleme
   }
 
   async getByNameAsync(name: string): Promise<Label | null> {
-    const query = this.collectionRef().where("normalizedName", "==", name).limit(1);
+    const query = this.collectionRef().where("normalizedName", "==", NormalizedName.create(name).getValue()).limit(1);
     const snapshot = await query.get();
     return snapshot.empty ? null : (snapshot.docs[0].data());
   }
