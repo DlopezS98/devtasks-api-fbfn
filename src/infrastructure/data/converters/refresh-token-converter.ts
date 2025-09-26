9e4b09ca7dca10286c91f0e79e7cb93f907ec131
DlopezS98 <01dlopezs98@gmail.com>
2025-09-26 12:03:53 -0600

    implement custom firestore converter for refresh tokens

--- src/infrastructure/data/converters/refresh-token-converter.ts
+++ src/infrastructure/data/converters/refresh-token-converter.ts
@@ -1,11 +1,25 @@
-import RefreshToken from "@Domain/entities/refresh-token.entity";
-import { FirestoreDataConverter } from "firebase-admin/firestore";
+import RefreshToken, { RefreshTokenProps } from "@Domain/entities/refresh-token.entity";
 import * as admin from "firebase-admin";
+import { ICustomFirestoreConverter } from "@Infrastructure/asbtractions/icustom-firestore-converter";
+import { UpdateData } from "firebase-admin/firestore";
 
 import FirestoreUtils from "../firestore.utils";
 
 /* eslint-disable require-jsdoc */
-export default class RefreshTokenConverter implements FirestoreDataConverter<RefreshToken> {
+export default class RefreshTokenConverter implements ICustomFirestoreConverter<RefreshToken, RefreshTokenProps> {
+  toUpdateObject(model: RefreshToken): UpdateData<RefreshTokenProps> {
+    return {
+      userId: model.userId,
+      tokenHash: model.tokenHash,
+      expiresAt: model.expiresAt,
+      createdAt: model.createdAt,
+      revokedAt: model.revokedAt,
+      createdByIp: model.createdByIp,
+      userAgent: model.userAgent,
+      deviceName: model.deviceName,
+    };
+  }
+
   toFirestore(entity: RefreshToken): FirebaseFirestore.DocumentData {
     return {
       userId: entity.userId,
