38a87e8c6ceb859440de926346ed9ac0da426f8b
DlopezS98 <01dlopezs98@gmail.com>
2025-10-13 22:34:47 -0600

    Add UserMapper class for mapping User entities to MongoDB documents

---
+++ functions/src/infrastructure-mongo/data/mapper/user.mapper.ts
@@ -0,0 +1,34 @@
+import { OptionalUnlessRequiredId, WithId } from "mongodb";
+import User, { UserProps } from "@Domain/entities/user.entity";
+import BaseMapper from "@Infrastructure/Mongo/abstractions/base-mapper";
+import { MongoDocument } from "@Infrastructure/Mongo/models/mongo-document";
+
+export default class UserMapper extends BaseMapper<User, UserProps> {
+  override toDocument(user: User): OptionalUnlessRequiredId<MongoDocument<UserProps>> {
+    return {
+      displayName: user.displayName,
+      email: user.email,
+      passwordHash: user.passwordHash,
+      passwordSalt: user.passwordSalt,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      isActive: user.isActive,
+    };
+  }
+
+  override fromDocument(doc: WithId<MongoDocument<UserProps>>): User
+  override fromDocument(doc: OptionalUnlessRequiredId<MongoDocument<UserProps>>): User {
+    const id = !doc._id ? "" : doc._id.toHexString();
+
+    return new User({
+      id,
+      displayName: doc.displayName,
+      email: doc.email,
+      passwordHash: doc.passwordHash,
+      passwordSalt: doc.passwordSalt,
+      createdAt: doc.createdAt,
+      updatedAt: doc.updatedAt,
+      isActive: doc.isActive,
+    });
+  }
+}
