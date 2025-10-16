b785385ceaebf58aa3b1c43933be3ff9f80e8e78
DlopezS98 <01dlopezs98@gmail.com>
2025-10-15 22:04:08 -0600

    refactor(tasks): integrate FactoryMapper in constructor for improved task entity mapping

--- functions/src/infrastructure-mongo/data/repositories/tasks.repository.ts
+++ functions/src/infrastructure-mongo/data/repositories/tasks.repository.ts
@@ -3,13 +3,14 @@ import Task, { TaskProps } from "@Domain/entities/task.entity";
 import TaskLabel from "@Domain/entities/task-label.entity";
 
 import { IMongoContext } from "../mongo.context";
+import FactoryMapper from "../mapper/factory-mapper";
 
 import MongoRepository from "./mongo.repository";
 import UnitOfWork from "./unit-of-work";
 
 export default class TasksRepository extends MongoRepository<Task, TaskProps> implements ITasksRepository {
   constructor(context: IMongoContext, uow?: UnitOfWork) {
-    super(context, Task.empty, uow);
+    super(context, Task.empty, FactoryMapper.createMapper(Task.empty()), uow);
   }
 
   getByUserAsync(userId: string): Promise<Task[]> {
