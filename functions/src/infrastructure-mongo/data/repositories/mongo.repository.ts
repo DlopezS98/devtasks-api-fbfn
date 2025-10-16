203fd7f0ae424d5630aefda11b5cbfabdba2b2b5
DlopezS98 <01dlopezs98@gmail.com>
2025-10-16 12:16:40 -0600

    refactor(mongo.repository): implement updateAsync, updateSingleAsync, and updateManyAsync methods for document updates

--- functions/src/infrastructure-mongo/data/repositories/mongo.repository.ts
+++ functions/src/infrastructure-mongo/data/repositories/mongo.repository.ts
@@ -1,4 +1,4 @@
-import { BSON, Collection, Filter } from "mongodb";
+import { AnyBulkWriteOperation, BSON, Collection, Filter, WithId } from "mongodb";
 import { IAsyncRepository } from "@Domain/abstractions/repositories/iasync-repository";
 import BaseEntity, { BaseEntityProps } from "@Domain/entities/base-entity";
 import { Query, PagedResult } from "@Domain/core/query";
@@ -61,9 +61,43 @@ export default class MongoRepository<TEntity extends BaseEntity, TProps extends 
 
   updateAsync(entity: TEntity): Promise<void>;
   updateAsync(entities: TEntity[]): Promise<void>;
-  updateAsync(entities: unknown): Promise<void> {
-    throw new Error("Method not implemented.");
+  updateAsync(entities: TEntity | TEntity[]): Promise<void> {
+    return Array.isArray(entities) ? this.updateManyAsync(entities) : this.updateSingleAsync(entities);
   }
+
+  async updateSingleAsync(entity: TEntity): Promise<void> {
+    const collection = this.getCollection();
+    const filter = { _id: new BSON.ObjectId(entity.id) } as Filter<MongoDocument<TProps>>;
+    if (this.uow) {
+      const doc = this.mapper.toPartialDocument(entity);
+      await this.uow.updateAsync(filter, doc, collection);
+      return;
+    }
+
+    const doc = this.mapper.toPartialDocument(entity);
+    await collection.updateOne(filter, { $set: doc });
+  }
+
+  async updateManyAsync(entities: TEntity[]): Promise<void> {
+    const collection = this.getCollection();
+    const docs = entities.map((entity) => {
+      return { _id: new BSON.ObjectId(entity.id), ...this.mapper.toPartialDocument(entity) };
+    }) as WithId<Partial<MongoDocument<TProps>>>[];
+    if (this.uow) {
+      await this.uow.updateManyAsync(docs, collection);
+      return;
+    }
+
+    const operations: AnyBulkWriteOperation<MongoDocument<TProps>>[] = docs.map((doc) => {
+      const { _id: id, ...rest } = doc;
+      const filter = { _id: id } as Filter<MongoDocument<TProps>>;
+      const update = { $set: rest as unknown as Partial<MongoDocument<TProps>> };
+      return { updateOne: { filter, update } };
+    });
+
+    await collection.bulkWrite(operations);
+  }
+
   deleteAsync(entity: TEntity): Promise<void>;
   deleteAsync(entities: TEntity[]): Promise<void>;
   deleteAsync(entities: unknown): Promise<void> {
