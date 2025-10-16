4ab36813ee69bf8532e2aae128a818c7c61d9c12
DlopezS98 <01dlopezs98@gmail.com>
2025-10-16 12:14:47 -0600

    refactor(mongo-document): remove unnecessary namespace property from MongoDocument type

--- functions/src/infrastructure-mongo/models/mongo-document.ts
+++ functions/src/infrastructure-mongo/models/mongo-document.ts
@@ -1,3 +1,3 @@
 import { BaseEntityProps } from "@Domain/entities/base-entity";
 
-export type MongoDocument<TProps extends BaseEntityProps> = Omit<TProps, "id" | "namespace">;
+export type MongoDocument<TProps extends BaseEntityProps> = Omit<TProps, "id">;
